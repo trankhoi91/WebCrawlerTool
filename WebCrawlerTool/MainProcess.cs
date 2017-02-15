e292b684d1c0ea50f40082ad44c39bfa33f97ea5
tran khoi <tran.khoiit91@oulook.com>
2017-02-15 23:49:27 +0700

    init variable

---
+++ WebCrawlerTool/MainProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawlerTool
+{
+    class MainProcess
+    {
+        // link phong vu
+        string urlCPU_PV = "http://phongvu.vn/thiet-bi-tin-hoc/cpu-bo-vi-xu-ly-101c.html";
+        string urlMain_PV = "http://phongvu.vn/thiet-bi-tin-hoc/mainboard-bo-mach-chu-100c.html";
+        string urlMain_page_PV = "http://phongvu.vn/thiet-bi-tin-hoc/mainboard-bo-mach-chu-100/cpage-2.html";
+
+
+
+
+    }
+}
