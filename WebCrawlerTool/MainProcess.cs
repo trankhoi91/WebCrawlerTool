a6544ddcb4030ba2457936316b0c54ecf1022ebd
tran khoi <tran.khoiit91@oulook.com>
2017-02-17 00:05:59 +0700

    update link source

--- WebCrawlerTool/MainProcess.cs
+++ WebCrawlerTool/MainProcess.cs
@@ -9,12 +9,42 @@ namespace WebCrawlerTool
     class MainProcess
     {
         // link phong vu
+        // link chuyen trang /cpage-2.html
         string urlCPU_PV = "http://phongvu.vn/thiet-bi-tin-hoc/cpu-bo-vi-xu-ly-101c.html";
         string urlMain_PV = "http://phongvu.vn/thiet-bi-tin-hoc/mainboard-bo-mach-chu-100c.html";
-        string urlMain_page_PV = "http://phongvu.vn/thiet-bi-tin-hoc/mainboard-bo-mach-chu-100/cpage-2.html";
+        string urlRAM_PV = "http://phongvu.vn/thiet-bi-tin-hoc/ram-bo-nho-104c.html";
+        string url_internal_notebook_PV = "http://phongvu.vn/thiet-bi-tin-hoc/thiet-bi-luu-tru/hdd-internal/for-notebook-1602c.html";
+        string urlSSD_PV = "http://phongvu.vn/thiet-bi-tin-hoc/thiet-bi-luu-tru/hdd-internal/ssd-1560c.html";
+        string urlHDD_PV = "http://phongvu.vn/thiet-bi-tin-hoc/thiet-bi-luu-tru/hdd-internal/wd-304c.html";
+        string url_external_PV = "http://phongvu.vn/thiet-bi-tin-hoc/thiet-bi-luu-tru/hdd-external/wd-1419c.html";
+        string urlVGA_PV = "http://phongvu.vn/thiet-bi-tin-hoc/vga-card-man-hinh-109c.html";
+        string urlMonitor_PV = "http://phongvu.vn/thiet-bi-tin-hoc/monitor-112c.html";
+
+        //phuoc son 
+        string urlMain_PS = "http://tinhocphuocson.vn/mainboard-d124.html";
+        string urlCPU_PS = "http://tinhocphuocson.vn/cpu-d207.html";
+        string urlRAM_PS = "http://tinhocphuocson.vn/ram-bo-nho-d123.html";
+        string urlVGA_PS = "http://tinhocphuocson.vn/card-man-hinh-d201.html";
+        string urlSSD_PS = "http://tinhocphuocson.vn/o-cung-ssd-d125.html";
+        string urlHDD_PS = "http://tinhocphuocson.vn/hdd-o-cung-co-d162.html";
+        string urlMonitor_PS = "http://tinhocphuocson.vn/man-hinh-lcd-d127.html";
+
+        // nam an 
+        string urlMain_Asrock_2011V3_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/asrock-intel/socket-2011-v3.html";
+        string urlMain_Asrock_1151_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/asrock-intel/socket-1153.html";
+        string urlMain_Giga_2011V3_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/gigabyte-intel/socket-2011-32014-09-21-07-07-29_.html";
+        string urlMain_Giga_1151_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/gigabyte-intel/socket-1151-gigabyte.html";
+        string urlMain_MSI_2011V3_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/msi2013-09-23-04-40-43_/socket-2011-3.html";
+        string urlMain_MSI_1151_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/msi2013-09-23-04-40-43_/socket-1152.html";
+        string urlMain_Asus_1151_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/asus-mainboard/socket-1151-asus.html";
+        string urlMain_Asus_2011V3_NA = "http://www.vitinhnaman.com/component/virtuemart/mainboard-for-intel/asus-mainboard/socket-2011-v3-x99.html";
 
 
 
-
+        public int checkNumofPage(string link)
+        {
+            int count = 0;
+            return count;
+        }
     }
 }
